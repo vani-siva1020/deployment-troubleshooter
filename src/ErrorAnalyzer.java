192045434ab363e3e36742e04dd71c6366c7d823
Shrii <sivavani359@gmail.com>
2026-06-13 18:05:29 +0530

    Implement error log analysis in ErrorAnalyzer

--- src/ErrorAnalyzer.java
+++ src/ErrorAnalyzer.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class ErrorAnalyzer {
+
+    public HashMap<String, ArrayList<Integer>>
+    analyze(List<String> logs) {
+
+        HashMap<String, ArrayList<Integer>>
+                errorMap = new HashMap<>();
+
+        for(int i = 0; i < logs.size(); i++) {
+
+            String line = logs.get(i);
+
+            if(line.startsWith("ERROR")) {
+
+                String error =
+                        line.substring(6);
+
+                errorMap.putIfAbsent(
+                        error,
+                        new ArrayList<>());
+
+                errorMap.get(error)
+                        .add(i + 1);
+            }
+        }
+
+        return errorMap;
+    }
+}
