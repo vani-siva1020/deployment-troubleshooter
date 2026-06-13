63cc25517aacd146f4d2d08d86228931eebcf0e8
Shrii <sivavani359@gmail.com>
2026-06-13 18:04:51 +0530

    Implement LogReader class for reading log files
    Added a LogReader class to read logs from a file.

--- src/LogReader.java
+++ src/LogReader.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import java.util.*;
 
+public class LogReader {
+
+    public List<String> readLogs(String fileName)
+            throws IOException {
+
+        List<String> logs =
+                new ArrayList<>();
+
+        BufferedReader br =
+                new BufferedReader(
+                        new FileReader(fileName));
+
+        String line;
+
+        while((line = br.readLine()) != null) {
+
+            logs.add(line);
+        }
+
+        br.close();
+
+        return logs;
+    }
+}
